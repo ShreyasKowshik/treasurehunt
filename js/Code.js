910ca519acf511257d7e369ef129629c6a85eefa
ShreyasKowshik <76945179+ShreyasKowshik@users.noreply.github.com>
2021-10-18 12:40:15 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,24 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("REVBAILA", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+    
+        fill("white")
+        textSize(15)
+        text("CUTNIFON", 700,130)
+        fill("lightblue")
+        text("Hint: Performs a perticular task !!", 700,150)
+    
+        fill("white")
+        textSize(15)
+        text("ATEDASBA", 500,240)
+        fill("lightblue")
+        text("Hint: Shows all information !!", 500,260)
+}
